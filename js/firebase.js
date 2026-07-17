61dc7a4bffd90aeeb4cd0b7ca066ad5218cace4c
suryansh-cse <suryansh.cse@gmail.com>
2026-07-18 02:13:59 +0530

    data

---
+++ js/firebase.js
@@ -0,0 +1,20 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDLTgPKhHvsrHV1lBnjvH9IqtJ61g-UVME",
+  authDomain: "infotris.firebaseapp.com",
+  projectId: "infotris",
+  storageBucket: "infotris.firebasestorage.app",
+  messagingSenderId: "639697612588",
+  appId: "1:639697612588:web:554d4203c1fbc55a984689",
+  measurementId: "G-GW1SZKVKL1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+console.log("🔥 Firebase Connected Successfully!");
+
+export { app };
