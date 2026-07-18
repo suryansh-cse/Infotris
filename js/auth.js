d8e91b58ee2ab774dbdd78b9b312137e3bfc9395
suryansh-cse <suryansh.cse@gmail.com>
2026-07-18 13:09:12 +0530

    idk man

--- js/auth.js
+++ js/auth.js
@@ -6,3 +6,17 @@ import {
     signOut,
     onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+
+console.log("✅ Auth.js Loaded");
+
+const form = document.getElementById("authForm");
+
+form.addEventListener("submit", handleAuthSubmit);
+
+async function handleAuthSubmit(event) {
+
+    event.preventDefault();
+
+    console.log("Submit Clicked");
+
+}
